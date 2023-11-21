1557432939a38aa401e6ee1f635019e7fe3ff552
Pratham Agarwal <gargpratham71@gmail.com>
2023-11-21 23:37:36 +0530

    Testimonials

---
+++ components/Testimonials.js
@@ -0,0 +1,13 @@
+// components/Testimonials.js
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
+import Image from "next/image";
+
+const Testimonials = () => {
+  return (
+    <Text fontSize="xl" fontWeight="bold" fontFamily="Aladin">
+      BECOME ONE OF MY NEXT SUCCESS STORIES LIKE…
+    </Text>
+  );
+};
+
+export default Testimonials;

db9c81ab3c52eac3da983057f06988d04f1c6f86
Pratham Agarwal <gargpratham71@gmail.com>
2023-11-23 02:02:37 +0530

    Index Changed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 // index.js
 import { Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
-import Testimonials from "@/components/Testimonials";
 import HeroSection from "@/components/HeroSection";
 import PaymentGateway from "@/components/PaymentGateway";
 
@@ -9,10 +8,8 @@ const Index = () => {
   return (
     <Box>
       <Navbar />
-      <HeroSection/>
-      <PaymentGateway />
+      <HeroSection />
       {/* Add the rest of your content here */}
-      <Testimonials />
     </Box>
   );
 };

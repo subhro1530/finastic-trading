b8e7cb9bed7c7efa662651359d63c543ab4b804a
subhro1530 <shaswata.ssaha@gmail.com>
2023-11-23 02:05:40 +0530

    folder modified, end for today

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import { Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import PaymentGateway from "@/components/PaymentGateway";
+import Testimonials from "@/components/Testimonials";
 
 const Index = () => {
   return (
     <Box>
       <Navbar />
       <HeroSection />
+      <Testimonials/>
       {/* Add the rest of your content here */}
     </Box>
   );

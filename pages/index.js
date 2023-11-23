5ea5435d6a2c8c238ee6217fd92f868ccd301285
subhro1530 <shaswata.ssaha@gmail.com>
2023-11-24 00:17:32 +0530

    initial changes today

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,20 @@ import { Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import PaymentGateway from "@/components/PaymentGateway";
-import Testimonials from "@/components/Testimonials";
+import Footer from "@/components/Footer";
+// import Testimonials from "@/components/Testimonials";
+import dynamic from "next/dynamic";
+const Testimonials = dynamic(() => import("@/components/Testimonials"), {
+  ssr: false,
+});
 
 const Index = () => {
   return (
     <Box>
       <Navbar />
       <HeroSection />
-      <Testimonials/>
+      <Testimonials />
+      <Footer />
       {/* Add the rest of your content here */}
     </Box>
   );

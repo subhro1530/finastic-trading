ce7eb98dba2ec5e934d0a3cb12c5e64f9a4880ee
Pratham Agarwal <gargpratham71@gmail.com>
2023-11-23 18:29:10 +0530

    Testimonials Final

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,92 +1,90 @@
 // components/Testimonials.js
-import { Box, Text, VStack, HStack, Button } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+// components/Testimonials.js
+import { useRef, useEffect, useState } from "react";
+import { Box, Text, VStack, Button } from "@chakra-ui/react";
 import styles from "@/styles/testimonials.module.css";
 
-const Testimonials = () => {
+const Testimonial = ({ name, profession, quote, videoSrc, alignment }) => {
   const [isClient, setIsClient] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+  const boxRef = useRef(null);
 
   useEffect(() => {
     setIsClient(true);
+
+    const handleScroll = () => {
+      if (boxRef.current) {
+        const { top } = boxRef.current.getBoundingClientRect();
+        setIsVisible(top < window.innerHeight);
+        if (isVisible && !isDone) {
+          // Set isDone to true when the component is visible
+          setIsDone(true);
+        }
+      }
+    };
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+    // Call handleScroll once to check the initial position
+    handleScroll();
+    // Remove the event listener when the component is unmounted
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const animationClass =
+    isVisible && !isDone
+      ? alignment === "left"
+        ? styles.leftSlideAnimation
+        : styles.rightSlideAnimation
+      : "";
+
   return (
-    <VStack>
-      <Box>
-        <Text
-          fontSize="2rem"
-          fontWeight="bold"
-          fontFamily="Aladin"
-          textAlign="center"
+    <Box
+      ref={boxRef}
+      display="flex"
+      alignItems="center"
+      className={animationClass}
+    >
+      {alignment === "left" && (
+        <Box
+          flex="1"
+          mr="4"
           style={{
-            textShadow: "2px 2px 4px rgba(22,242,139,0.2)",
+            margin: "1rem 3rem",
           }}
         >
-          BECOME ONE OF MY{" "}
-          <span style={{ color: "rgb(22,242,139)" }}> NEXT SUCCESS</span>{" "}
-          <Text>STORIES LIKE…</Text>
-        </Text>
-        <Box display="flex" alignItems="center">
-          <Box
-            flex="1"
-            mr="4"
-            style={{
-              margin: "1rem 3rem",
-            }}
-          >
-            {isClient && (
-              <iframe
-                width="100%"
-                height="315"
-                src="https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID"
-                title="YouTube Video"
-                allowFullScreen
-                style={{
-                  borderRadius: "10px",
-                  boxShadow: "1px 1px 1px 2px rgba(0,0,0,.5)",
-                }}
-              ></iframe>
-            )}
-          </Box>
-          <VStack align="start" flex="2">
-            <Text fontSize="2rem" fontWeight="bold" color="rgb(13,201,106)">
-              Shaswata Saha
-            </Text>
-            <Text fontSize="1.3rem" color="black">
-              Experienced Trading Strategist, Financial Markets Pro, Risk
-              Management Specialist
-            </Text>
-            <Text
-              fontSize="md"
-              color="gray.500"
-              fontStyle="italic"
-              style={{ marginTop: "1.5rem" }}
-            >
-              &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum condimentum, mauris at fermentum&quot;
-            </Text>
-          </VStack>
+          {isClient && (
+            <iframe
+              width="100%"
+              height="315"
+              src={videoSrc}
+              title="YouTube Video"
+              allowFullScreen
+              style={{
+                borderRadius: "10px",
+                boxShadow: "1px 1px 1px 2px rgba(0,0,0,.5)",
+              }}
+            ></iframe>
+          )}
         </Box>
-      </Box>
-      <Box display="flex" alignItems="center">
-        <VStack align="start" flex="2" style={{ marginLeft: "1rem" }}>
-          <Text fontSize="2rem" fontWeight="bold" color="rgb(13,201,106)">
-            Pratham Agarwal
-          </Text>
-          <Text fontSize="1.3rem" color="black">
-            +400k Profits in Trading Career, Financial Markets Success, Expert
-            Risk Manager
-          </Text>
-          <Text
-            fontSize="md"
-            color="gray.500"
-            fontStyle="italic"
-            style={{ marginTop: "1.5rem" }}
-          >
-            &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vestibulum condimentum, mauris at fermentum&quot;
-          </Text>
-        </VStack>
+      )}
+      <VStack align="start" flex="2" style={{ margin: "1rem" }}>
+        <Text fontSize="2rem" fontWeight="bold" color="rgb(13,201,106)">
+          {name}
+        </Text>
+        <Text fontSize="1.3rem" color="black">
+          {profession}
+        </Text>
+        <Text
+          fontSize="md"
+          color="gray.500"
+          fontStyle="italic"
+          style={{ marginTop: "1.5rem" }}
+        >
+          &quot;{quote}&quot;
+        </Text>
+      </VStack>
+      {alignment === "right" && (
         <Box
           flex="1"
           ml="4"
@@ -98,7 +96,7 @@ const Testimonials = () => {
             <iframe
               width="100%"
               height="315"
-              src="https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID"
+              src={videoSrc}
               title="YouTube Video"
               allowFullScreen
               style={{
@@ -108,48 +106,49 @@ const Testimonials = () => {
             ></iframe>
           )}
         </Box>
-      </Box>
-      <Box display="flex" alignItems="center">
-        <Box
-          flex="1"
-          mr="4"
-          style={{
-            margin: "1rem 3rem",
-            marginLeft: "1rem",
-          }}
-        >
-          {isClient && (
-            <iframe
-              width="100%"
-              height="315"
-              src="https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID"
-              title="YouTube Video"
-              allowFullScreen
-              style={{
-                borderRadius: "10px",
-                boxShadow: "1px 1px 1px 2px rgba(0,0,0,.5)",
-              }}
-            ></iframe>
-          )}
-        </Box>
-        <VStack align="start" flex="2">
-          <Text fontSize="2rem" fontWeight="bold" color="rgb(13,201,106)">
-            Henda Mendis
-          </Text>
-          <Text fontSize="1.3rem" color="black">
-            Financial Maestro, Risk Management Expert, Investment Virtuoso
-          </Text>
-          <Text
-            fontSize="md"
-            color="gray.500"
-            fontStyle="italic"
-            style={{ marginTop: "1.5rem" }}
-          >
-            &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vestibulum condimentum, mauris at fermentum&quot;
-          </Text>
-        </VStack>
-      </Box>
+      )}
+    </Box>
+  );
+};
+
+const Testimonials = () => {
+  return (
+    <VStack>
+      <Text
+        fontSize="2rem"
+        fontWeight="bold"
+        fontFamily="Aladin"
+        textAlign="center"
+        style={{
+          margin: "2rem",
+          textShadow: "2px 2px 4px rgba(22,242,139,0.2)",
+        }}
+      >
+        BECOME ONE OF MY{" "}
+        <span style={{ color: "rgb(22,242,139)" }}> NEXT SUCCESS</span>{" "}
+        <Text>STORIES LIKE…</Text>
+      </Text>
+      <Testimonial
+        name="Shaswata Saha"
+        profession="Experienced Trading Strategist"
+        quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum, mauris at fermentum"
+        videoSrc="https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID"
+        alignment="left"
+      />
+      <Testimonial
+        name="Pratham Agarwal"
+        profession="+400k Profits in Trading Career"
+        quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum, mauris at fermentum"
+        videoSrc="https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID"
+        alignment="right"
+      />
+      <Testimonial
+        name="Henda Mendis"
+        profession="Financial Maestro, Risk Management Expert, Investment Virtuoso"
+        quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum, mauris at fermentum"
+        videoSrc="https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID"
+        alignment="left"
+      />
       <Button
         align="center"
         style={{
@@ -163,6 +162,7 @@ const Testimonials = () => {
           fontFamily: "Aladin",
           fontWeight: "normal",
           margin: "2rem",
+          padding: "1rem",
           boxShadow:
             "6px 6px 18px 0px rgba(47.81203804347826, 226.7375, 126.38365394612467, 0.5)",
         }}
@@ -178,4 +178,5 @@ const Testimonials = () => {
     </VStack>
   );
 };
+
 export default Testimonials;
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,18 @@ import { Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import PaymentGateway from "@/components/PaymentGateway";
-import Testimonials from "@/components/Testimonials";
+// import Testimonials from "@/components/Testimonials";
+import dynamic from "next/dynamic";
+const Testimonials = dynamic(() => import("@/components/Testimonials"), {
+  ssr: false,
+});
 
 const Index = () => {
   return (
     <Box>
       <Navbar />
       <HeroSection />
-      <Testimonials/>
+      <Testimonials />
       {/* Add the rest of your content here */}
     </Box>
   );
